72f35311820703f9e7dffe7544d3e612d3f0a372
Ivan Krasilnikov <krasivan93@gmail.com>
2020-11-23 20:04:30 +0200

    Final version. Add supervisord.

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-from fastapi import FastAPI, File, UploadFile, Request, HTTPException
+from fastapi import FastAPI, File, HTTPException
 from fastapi.responses import FileResponse
 
-import mimetypes
 import uvicorn
 import hashlib
-import shutil
 import os
 import magic
 
 app = FastAPI()
 
 
-@app.post("/upload")
-async def image(file: bytes = File(...)):
+@app.post("/upload", status_code=201)
+def upload_file(file: bytes = File(...)):
     hash_object = hashlib.md5(file)
     file_hash = hash_object.hexdigest()
 
     save_dir = f"store/{file_hash[:2]}"
-    print(save_dir, 'save path')
     os.makedirs(save_dir, exist_ok=True)
     save_path = save_dir + f'/{file_hash}'
 
@@ -28,7 +25,7 @@ async def image(file: bytes = File(...)):
 
 
 @app.get("/download")
-async def main(file_hash: str):
+def download_file(file_hash: str):
     file_path = f'store/{file_hash[:2]}/{file_hash}'
 
     if os.path.exists(file_path):
@@ -38,5 +35,16 @@ async def main(file_hash: str):
         return HTTPException(status_code=404, detail="Item not found")
 
 
+@app.delete("/delete", status_code=201)
+def delete_file(file_hash: str):
+    file_path = f'store/{file_hash[:2]}/{file_hash}'
+
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        return
+    else:
+        return HTTPException(status_code=404, detail="Item not found")
+
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
